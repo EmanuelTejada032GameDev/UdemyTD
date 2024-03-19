372d14c444961fb8a524fc65ca9cc20496a0d295
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2024-03-19 09:12:36 -0400

    simple comment on resource generator
    add simple comment and different logic from course and my custom to generate resource by nearbyresourcenodes

--- udemytd/Assets/Scripts/ResourceGenerator.cs
+++ udemytd/Assets/Scripts/ResourceGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ResourceGenerator : MonoBehaviour
@@ -6,25 +7,39 @@ public class ResourceGenerator : MonoBehaviour
 
     private float _timer;
     [SerializeField] private float _timeToGenerateResource;
-    private static int reachedResourceNodes = 0;
+    private static int _reachedResourceNodes = 0;
 
     private void Start()
     {
-        reachedResourceNodes = GetNearbyResourceNodes(transform.position, _resourceGeneratorConfig);
-
-        Mathf.Clamp(reachedResourceNodes, 0, _resourceGeneratorConfig.MaxResourceNodes);
-        if (reachedResourceNodes == 0) enabled = false;
-
-        float baseTime = _resourceGeneratorConfig.TimeToGenerateResource; // Base time for one unit of resource
-        float scaleFactor = 1.2f; // Scaling factor to adjust time
-        _timeToGenerateResource = baseTime / (1 + scaleFactor * reachedResourceNodes);
+       
     }
 
     private void Awake()
     {
         _resourceGeneratorConfig = GetComponent<BuildingTypeHolder>().BuildingTypeSO.resourgeGenerationConfig;
+        _reachedResourceNodes = GetNearbyResourceNodes(transform.position, _resourceGeneratorConfig);
+
+        SetTimeToGenerateResourceBasedOnReachedResourceNodes();
+     
     }
 
+    private void SetTimeToGenerateResourceBasedOnReachedResourceNodes()
+    {
+        if (_reachedResourceNodes == 0)
+        {
+            _timeToGenerateResource = 0;
+            enabled = false;
+            return;
+        }
+
+        // This logic to determine _timeTOGenerateResource is not like course class 17 , min 21
+        float baseTime = _resourceGeneratorConfig.TimeToGenerateResource; // Base time for one unit of resource
+        float scaleFactor = 1.2f; // Scaling factor to adjust time
+        _timeToGenerateResource = baseTime / (1 + scaleFactor * _reachedResourceNodes);
+
+        //this is 
+        //_timeToGenerateResource = (_resourceGeneratorConfig.TimeToGenerateResource / 2f) + _resourceGeneratorConfig.TimeToGenerateResource * (1 - (float)_reachedResourceNodes / _resourceGeneratorConfig.MaxResourceNodes);
+    }
 
     void Update()
     {
@@ -55,6 +70,8 @@ public class ResourceGenerator : MonoBehaviour
             }
         }
 
+        Mathf.Clamp(nearbyResources, 0, resourceGeneratorConfig.MaxResourceNodes);
+
         return nearbyResources;
     }
 
@@ -65,11 +82,11 @@ public class ResourceGenerator : MonoBehaviour
 
     public float GetTimeNormalized()
     {
-        return _timer / _timeToGenerateResource;
+        return _timeToGenerateResource == 0? 0: _timer / _timeToGenerateResource;
     }
 
     public float GetAmountGeneratedPerSecond()
     {
-        return 1 / _timeToGenerateResource;
+        return _timeToGenerateResource == 0? 0 : 1 / _timeToGenerateResource;
     }
 }
