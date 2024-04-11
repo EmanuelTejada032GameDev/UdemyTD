76fe1aa1f4dd046467a81f8549de61fabee666c3
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2024-04-11 11:43:52 -0400

    Enemy  building target logic
    Enemy can target closest building , if no building around enemy target the HQ
    if another building gets in enemy detection radius the enemy change target to that building

--- udemytd/Assets/Scripts/Enemy.cs
+++ udemytd/Assets/Scripts/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
     private Rigidbody2D _rigidBody2d;
     [SerializeField] private float _moveSpeed = 6f;
 
+    private float _lookForTargetTimer;
+    private float _lookForTargetMaxTime;
 
     public static Enemy Create(Vector3 position)
     {
@@ -22,8 +24,31 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        Vector3 moveDir = (_targetTransform.position - transform.position).normalized;
-        _rigidBody2d.velocity = moveDir * _moveSpeed;   
+        HandleMovement();
+        HandleTargeting();
+    }
+
+    private void HandleMovement()
+    {
+        if (_targetTransform != null)
+        {
+            Vector3 moveDir = (_targetTransform.position - transform.position).normalized;
+            _rigidBody2d.velocity = moveDir * _moveSpeed;
+        }
+        else
+        {
+            LookForTargets();
+        }
+    }
+
+    private void HandleTargeting()
+    {
+        _lookForTargetTimer -= Time.deltaTime;
+        if (_lookForTargetTimer < 0f)
+        {
+            _lookForTargetTimer += _lookForTargetMaxTime;
+            LookForTargets();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -34,6 +59,46 @@ public class Enemy : MonoBehaviour
         {
             building.HealthSystem.TakeDamage(6);
             Destroy(gameObject);
+        }
+    }
+
+
+    private void LookForTargets()
+    {
+        float _maxRadiusTargetDetection = 10f;
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, _maxRadiusTargetDetection);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            Building building = collider2D.gameObject.GetComponent<Building>();
+            if(building != null)
+            {
+                if(_targetTransform == null)
+                {
+                    _targetTransform = building.transform;
+                }
+                else
+                {
+                    if(Vector3.Distance(transform.position, building.transform.position) < Vector3.Distance(transform.position, _targetTransform.position))
+                    {
+                        _targetTransform = building.transform;
+                    }
+                }
+            }
+        }
+
+        if(_targetTransform == null)
+        {
+
+            Building hqBuilding = BuildingManager.Instance.GetHQBuilding();
+            if (hqBuilding == null)
+            {
+                _rigidBody2d.velocity = Vector3.zero;
+                Destroy(gameObject, Random.Range(2.5f, 6f));
+                return;
+            }
+
+            _targetTransform = BuildingManager.Instance.GetHQBuilding().transform;
         }
     }
 }
