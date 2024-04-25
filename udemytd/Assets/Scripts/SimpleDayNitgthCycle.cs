5e586d87c366ed2bb11a8299393dd8a180775f7b
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2024-04-25 11:43:17 -0400

    day nigth cycle
    add day nigth cycle script that changes 2d global ligth colors within time
    update urp renderer in project settings graphics and quality

---
+++ udemytd/Assets/Scripts/SimpleDayNitgthCycle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SimpleDayNitgthCycle : MonoBehaviour
+{
+
+    [SerializeField] private Gradient _gradient;
+    [SerializeField] private float secondsPerDay; // Represents how much times represents 24h
+
+    private Light2D _2dLight;
+    private float _dayTime;
+    private float _dayTimeSpeed;
+
+
+
+    private void Awake()
+    {
+        _2dLight = GetComponent<Light2D>();
+        _dayTimeSpeed = 1 / secondsPerDay;
+    }
+
+    private void Update()
+    {
+        _dayTime += Time.deltaTime * _dayTimeSpeed;
+        _2dLight.color = _gradient.Evaluate(_dayTime % 1f);
+    }
+}
